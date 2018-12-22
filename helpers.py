ce84ff5a8d90a7deb26342951594fbcfd7383c38
Yuriy Kots <yuriy@gestalt.io>
2018-12-22 14:47:46 -0500

    Add load and reduce fashion mnist dataset functions to helpers.py

--- helpers.py
+++ helpers.py
@@ -1,3 +1,8 @@
+import tensorflow as tf
+from tensorflow import keras
+from random import shuffle
+
+import numpy as np
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
@@ -25,7 +30,7 @@ def print_length_and_shapes(train_images, train_labels, dev_images, dev_labels, 
     print('len(test_labels)')
     print(len(test_labels))
     print('test_labels')
-    print(train_labels)
+    print(test_labels)
 
 
 def show_one_image(img):
@@ -48,3 +53,39 @@ def show_multiple_images(images, labels, class_names):
         plt.xlabel(class_names[labels[i]])
 
     plt.show()
+
+
+def reduce_fashion_mnist_dataset(images, labels):
+    type_one = []
+    type_two = []
+
+    for i in labels:
+        if labels[i] == 1:
+            type_one.append(images[i])
+        elif labels[i] == 2:
+            type_two.append(images[i])
+
+    labels_type_one = [0] * len(type_one)
+    labels_type_two = [1] * len(type_two)
+
+    type_one_data = list(zip(type_one, labels_type_one))
+    type_two_data = list(zip(type_two, labels_type_two))
+
+    data_mix = type_one_data + type_two_data
+    shuffle(data_mix)
+
+    data_images, data_labels = zip(*data_mix)
+    data_images = np.array([np.array(img) for img in data_images])
+    data_labels = np.array([np.array(label) for label in data_labels])
+
+    return data_images, data_labels
+
+
+def load_fashion_mnist_dataset():
+    fashion_mnist = keras.datasets.fashion_mnist
+    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+    train_images, train_labels = reduce_fashion_mnist_dataset(train_images, train_labels)
+    test_images, test_labels = reduce_fashion_mnist_dataset(test_images, test_labels)
+
+    return train_images, train_labels, test_images, test_labels
