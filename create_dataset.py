e22f6454ed73839121ad12217bcfd7a2a9122d7c
Yuriy Kots <yuriy@gestalt.io>
2018-12-26 14:40:10 -0500

    Update dataset split ratio

--- create_dataset.py
+++ create_dataset.py
@@ -18,19 +18,19 @@ def create_numpy_array_data_record(paths, labels, file_name):
     data_labels.dump('{}_labels.npy'.format(file_name))
 
 
-img_paths = glob.glob('images/*/*.png')
+img_paths = glob.glob('images/*/*.jpg')
 img_labels = list(0 if 'false' in address else 1 for address in img_paths)
 img_paths_and_labels = list(zip(img_paths, img_labels))
 shuffle(img_paths_and_labels)
 img_paths, img_labels = zip(*img_paths_and_labels)
 
-# Train 60%, Dev 20%, Test 20%
-train_img_paths = img_paths[0: int(0.6*len(img_paths))]
-train_img_labels = img_labels[0: int(0.6*len(img_labels))]
-dev_img_paths = img_paths[int(0.6*len(img_paths)):int(0.8*len(img_paths))]
-dev_img_labels = img_labels[int(0.6*len(img_labels)):int(0.8*len(img_labels))]
-test_img_paths = img_paths[int(0.8*len(img_paths)):]
-test_img_labels = img_labels[int(0.8*len(img_labels)):]
+# Train 80%, Dev 10%, Test 10%
+train_img_paths = img_paths[0: int(0.8*len(img_paths))]
+train_img_labels = img_labels[0: int(0.8*len(img_labels))]
+dev_img_paths = img_paths[int(0.8*len(img_paths)):int(0.9*len(img_paths))]
+dev_img_labels = img_labels[int(0.8*len(img_labels)):int(0.9*len(img_labels))]
+test_img_paths = img_paths[int(0.9*len(img_paths)):]
+test_img_labels = img_labels[int(0.9*len(img_labels)):]
 
 create_numpy_array_data_record(train_img_paths, train_img_labels, 'dataset_train')
 create_numpy_array_data_record(dev_img_paths, dev_img_labels, 'dataset_dev')
